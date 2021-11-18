286c65b2e1353c06ddc88ea84feecb1865a85d3b
Rick Companje <rick@companje.nl>
2021-11-18 17:43:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,14 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header('Content-Type: text/plain');
-// header("Access-Control-Allow-Headers: Content-Type, Accept");
+// ini_set('display_errors', 'On');
 
-if (is_numeric($_SERVER['QUERY_STRING'])) {
-  $uri = getURI($_SERVER['QUERY_STRING']);
-  $pid = str_replace("/id/","/collectie/", $uri);
-  header("Location: $pid");
-}
+$id = $_GET['id'];
 
-function getURI($id) {
-  $query = "PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX id: <https://hetutrechtsarchief.nl/id/>
+if (is_numeric($id)) {
+  $db = new SQLite3('notaris.db');
+  $guid = $db->querySingle("SELECT guid from notaris where id=$id");
 
-    SELECT ?sub WHERE {
-      ?sub owl:sameAs ?obj .
-      values (?obj) { (id:r$id) }
-    } LIMIT 1";
-
-  $endpoint = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/hetutrechtsarchief/Dataset/services/Dataset/sparql";
-
-  $url = $endpoint . '?query=' . urlencode($query) . "&format=json";
-
-  $response = file_get_contents($url);
-  $json = json_decode($response);
-
-  return $json[0]->sub;
+  if ($guid) {
+    header("Location: https://hetutrechtsarchief.nl/collectie/$guid");
+  }
 }
 ?>
